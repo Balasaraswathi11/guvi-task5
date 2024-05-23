ad331a677620d0a030d34d78685b4269279f5a54
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-05-23 09:48:05 +0530

    Update resume_iterator.js

--- resume_iterator.js
+++ resume_iterator.js
@@ -1,111 +1,128 @@
 const data = {
-  fullName: "Balasaraswathi",
-  contactInformation: {
-    phoneNumber: "9787329631",
-    email: "twilightestrella11@gmail.com",
-    address: "3/13, Thoppu Street, Muthiahpuram, Thoothukudi."
-  },
-  objective: "To obtain a Javascript developer position that allows me to utilize my skills in coding and problem-solving to contribute to the success of the company.",
-  education: [
-    {
-      degree: "Bachelor of Arts in English",
-      institution: "St.Mary's college",
-      yearOfGraduation: "2023"
+    Name: "Balasaraswathi",
+    ContactInformation: {
+      PhoneNumber: "9787329631",
+      Email: "twilightestrella11@gmail.com",
+      Address: "3/13, Thoppu Street, Muthiahpuram, Thoothukudi."
+    },
+    Objective: "To obtain a Javascript developer position that allows me to utilize my skills in coding and problem-solving to contribute to the success of the company.",
+    Education: [
+      {
+        degree: "Bachelor of Arts in English",
+        institution: "St.Mary's college",
+        yearOfGraduation: "2023"
+      }
+    ],
+    Skills: [
+      "JavaScript",
+      "HTML/CSS",
+      "Git",
+      "Ms.Exel",
+      "Tally Erp9",
+      "Ms.Word",
+      "Ms.Powerpoint"
+    ],
+    Languages: [
+      "Tamil",
+      "English",
+      "Spanish",
+      "Chinese",
+      "Hindi"
+    ],
+    HobbiesInterests: [
+      "Reading",
+      "Coding",
+      "Editing"
+    ]
+  };
+//using for loop
+  const keys = Object.keys(data);
+for (let i = 0; i < keys.length; i++) {
+  const key = keys[i];
+  const value = data[key];
+  if (Array.isArray(value)) {
+    console.log(`${key}:`);
+    for (let j = 0; j < value.length; j++) {
+      console.log(`  - ${value[j]}`);
     }
-  ],
-  skills: [
-    "JavaScript",
-    "HTML/CSS",
-    "Git",
-    "Ms.Exel",
-    "Tally Erp9",
-    "Ms.Word",
-    "Ms.Powerpoint"
-  ],
-  languages: [
-    "Tamil",
-    "English",
-    "Spanish",
-    "Chinese",
-    "Hindi"
-  ],
-  hobbiesInterests: [
-    "Reading",
-    "Coding",
-    "Editing"
-  ]
-};
-
-// Using for loop to iterate over arrays
-for (let i = 0; i < data.skills.length; i++) {
-  console.log(data.skills[i]);
-}
-
-for (let i = 0; i < data.languages.length; i++) {
-  console.log(data.languages[i]);
-}
-
-for (let i = 0; i < data.hobbiesInterests.length; i++) {
-  console.log(data.hobbiesInterests[i]);
-}
-
-
-// Using for...in to iterate over object properties
-for (let key in data) {
-  console.log(`${key}: ${data[key]}`);
-}
-
-// Using for...in to iterate over nested objects
-for (let key in data.contactInformation) {
-  console.log(`${key}: ${data.contactInformation[key]}`);
-}
-
-// Using for...in to iterate over array of objects
-for (let i in data.education) {
-  for (let key in data.education[i]) {
-    console.log(`${key}: ${data.education[i][key]}`);
+  } else if (typeof value === 'object') {
+    console.log(`${key}:`);
+    const subKeys = Object.keys(value);
+    for (let j = 0; j < subKeys.length; j++) {
+      const subKey = subKeys[j];
+      console.log(`  ${subKey}: ${value[subKey]}`);
+    }
+  } else {
+    console.log(`${key}: ${value}`);
   }
 }
 
+//for in loop
 
-// Using for...of to iterate over arrays
-for (let skill of data.skills) {
-  console.log(skill);
-}
+for (const key in data) {
+    const value = data[key];
+    if (Array.isArray(value)) {
+      console.log(`${key}:`);
+      for (let j = 0; j < value.length; j++) {
+        console.log(`  - ${value[j]}`);
+      }
+    } else if (typeof value === 'object') {
+      console.log(`${key}:`);
+      for (const subKey in value) {
+        console.log(`  ${subKey}: ${value[subKey]}`);
+      }
+    } else {
+      console.log(`${key}: ${value}`);
+    }
+  }
 
-for (let language of data.languages) {
-  console.log(language);
-}
-
-for (let hobby of data.hobbiesInterests) {
-  console.log(hobby);
-}
-
-// Using for...of to iterate over array of objects
-for (let education of data.education) {
-  for (let key in education) {
-    console.log(`${key}: ${education[key]}`);
+//for of loop
+const entries = Object.entries(data);
+for (const [key, value] of entries) {
+  if (Array.isArray(value)) {
+    console.log(`${key}:`);
+    for (const item of value) {
+      console.log(`  - ${item}`);
+    }
+  } else if (typeof value === 'object') {
+    console.log(`${key}:`);
+    for (const subKey in value) {
+      console.log(`  ${subKey}: ${value[subKey]}`);
+    }
+  } else {
+    console.log(`${key}: ${value}`);
   }
 }
+//for each loop
+
+ const entries = Object.entries(data);
+
+  Object.entries(data).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      console.log(`${key}:`);
+      value.forEach(item => {
+        console.log(`  - ${item}`);
+      });
+    } else if (typeof value === 'object') {
+      console.log(`${key}:`);
+      Object.entries(value).forEach(([subKey, subValue]) => {
+        console.log(`  ${subKey}: ${subValue}`);
+      });
+    } else {
+      console.log(`${key}: ${value}`);
+    }
+  });
+  
 
 
-// Using forEach to iterate over arrays
-data.skills.forEach(skill => {
-  console.log(skill);
-});
+  
+  
 
-data.languages.forEach(language => {
-  console.log(language);
-});
+  
+ 
 
-data.hobbiesInterests.forEach(hobby => {
-  console.log(hobby);
-});
+  
+  
 
-// Using forEach to iterate over array of objects
-data.education.forEach(education => {
-  for (let key in education) {
-    console.log(`${key}: ${education[key]}`);
-  }
-});
-
+  
+ 
